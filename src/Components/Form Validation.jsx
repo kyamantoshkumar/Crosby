5883fa734434ec5fb1e37e59523f2c642bcaae9d
Mantosh <kyamantoshkumar@gmail.com>
2023-02-22 14:08:48 +0530

    Create Form Validation.jsx

---
+++ src/Components/Form Validation.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+
+function FormValidation() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+  const [errors, setErrors] = useState({});
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const errors = validateFormData(formData);
+    setErrors(errors);
+    if (Object.keys(errors).length === 0) {
+      // Submit form data to server
+    }
+  }
+
+  function validateFormData(formData) {
+    const errors = {};
+    if (!formData.name) {
+      errors.name = 'Name is required';
+    }
+    if (!formData.email) {
+      errors.email = 'Email is required';
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      errors.email = 'Email is invalid';
+    }
+    if (!formData.password) {
+      errors.password = 'Password is required';
+    } else if (formData.password.length < 6) {
+      errors.password = 'Password must be at least 6 characters long';
+    }
+    return errors;
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        {errors.name && <span>{errors.name}</span>}
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        {errors.email && <span>{errors.email}</span>}
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        {errors.password && <span>{errors.password}</span>}
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+export default FormValidation
