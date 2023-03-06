832893425e25f0528677076c2be59aad5772126c
Mantosh <kyamantoshkumar@gmail.com>
2023-03-06 21:42:19 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Appn from './Appn';
 
 // import { BrowserRouter } from 'react-router-dom';
 // import reportWebVitals from './reportWebVitals';
@@ -9,7 +10,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Appn />
   </React.StrictMode>
 );
 
